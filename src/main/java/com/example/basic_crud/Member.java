106909462d6a7b34fa919ef2d89ad3c80071d042
WonTaeJun <dnjsxonws94@gmail.com>
2025-12-02 19:27:01 +0900

    ✨ feat: 3.엔티티 설계

---
+++ src/main/java/com/example/basic_crud/Member.java
@@ -0,0 +1,43 @@
+package com.example.basic_crud;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 20, nullable = false)
+    private Long name;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false; //기본값 0으로 설정, DB 스키마에서 넣어줬으나 한 번더 확인차 넣어줌.
+
+    //JPA생성자
+    protected Member() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+}
